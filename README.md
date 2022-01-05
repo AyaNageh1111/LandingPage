bd9fc9507e7543b078c2e4956d05a2e4800b2497
unknown <s-aya.mphamed@zewailcity.edu.eg>
2022-01-05 22:43:55 +0200

    update_navigation bar

--- README.md
+++ README.md
@@ -6,9 +6,8 @@
 
 ## Instructions
 
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
 
-*First: Add 4th section
-*Secode: Add the 4 sections to navigation bar
-*Third: Change nav bar style
-*Fourth: Make the background color changble
-*Fifth: implement the active Bar
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
--- js/app.js
+++ js/app.js
@@ -1,76 +1,117 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
 
 /**
- * make list items equal to the number of sections by iterate on them
- * but I need to remove all items to avoid the duplicating
- */
-const navBar = document.getElementById("navbar__list");
-const CreatNavBar = () => {
-  document.querySelectorAll("section").forEach((section) => {
-    const listItem = `<li><a href="#${section.id}" data-nav="${section.id}" class="menu__link">${section.dataset.nav}</a></li>`;
-    navBar.insertAdjacentHTML("beforeend", listItem);
-  });
-};
-/**
- *  function to observe the section to specify which section on the viewport and its link
- *  loop over entries (sections)
- *  get active link by using the id of the section on viewport
- *  add active class to the section on viewport
- *  add active class to the section's link
- *  edit the hash of location manual cause i prevent default behavior
- *  remove active classes
- */
-
-window.onscroll = function() {
-	document.querySelectorAll("section").forEach(function(active) {
-    let activeLink = navBar.querySelector(`[data-nav=${active.id}]`);
-	if(active.getBoundingClientRect().top >= -400 && active.getBoundingClientRect().top <= 150){
-
-    active.classList.add("your-active-class");
-    activeLink.classList.add("active-link");
-
-    }
-    else{
-         active.classList.remove("your-active-class");
-         activeLink.classList.remove("active-link");
-    }
-	});
-}
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
 
 /**
- * when you click on nav links will go smoothly to the correct section
- */
-navBar.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (event.target.dataset.nav) {
-    document
-      .getElementById(`${event.target.dataset.nav}`)
-      .scrollIntoView({ behavior: "smooth" });
-    setTimeout(() => {
-      location.hash = `${event.target.dataset.nav}`;
-    }, 200);
-  }
-});
+ * Define Global Variables
+ * 
+*/
 
 
-CreatNavBar();
-
-
-// save the icon used to go to the top and the header in variables
-const toTop = document.getElementById("to-top");
-const header = document.querySelector(".page__header");
-
-// Clicking on the icon the document will scroll to the top smoothly
-toTop.addEventListener("click", () => {
-  document.documentElement.scrollTo({ 
-    top: 0,
-    behavior: "smooth" });
-});
 /**
- * disappear the header after 8 seconds and appear again when scrolling.
- * appearing the icon(to-top) after 800px to down
- */
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
 
 
 
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
 
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+
+// Set sections as active
+
+
+// // Dynamically add Sections
+
+// section_id =0;
+// // Event
+// const section =document.createElement('div');
+
+// const section_text = document.createTextNode('this is a new section');
+
+// section.appendChild(section_text);
+
+// document.body.appendChild(section);
+// section.classList.add('all_sections');
+
+// section.id=1;
+
+// const navigation=()=>{
+//     const nav__a =document.querySelectorAll('.nav__a li');
+
+//     nav__a.forEach((link, index)=>{
+//         link.style
+//     });
+// }
+
+// navigation();
+
+
+const section = document.querySelectorAll('section');
+const nav__li=document.querySelectorAll(' ul li');
+
+window.addEventListener('scroll', ()=>{
+    let current_active_section='';
+    section.forEach(section=>{
+        const top =section.offsetTop;
+        const height= section.clientHeight;
+        if (pageYOffset>=(top-height/4)){
+        current_active_section=section.getAttribute('id');
+       section.classList.add('your-active-class');
+        }
+        else
+        {
+            section.classList.remove('your-active-class');
+        }
+    })
+    
+    nav__li.forEach(nav__li=>{
+        console.log(nav__li.className);
+        nav__li.classList.remove('active');
+        if(nav__li.classList.contains(current_active_section)){
+            nav__li.classList.add('active');
+        }
+        })
+
+
+})
 
